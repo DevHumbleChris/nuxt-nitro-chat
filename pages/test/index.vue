a160a83701b9cb3ad93297965f032cc8c6e7a4ef
DevHumbleChris <dev.humblechris254@gmail.com>
2024-07-25 10:20:18 +0300

    test working

---
+++ pages/test/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Connection from "~/components/test/Connection.client.vue";
+
+definePageMeta({
+  auth: {
+    unauthenticatedOnly: false,
+  },
+});
+</script>
+
+<template>
+  <Connection />
+</template>
